b7eccffefbb92d12ac618533f4973b257dd7207d
John Wilkinson <johnpwilkinson@outlook.com>
2020-09-25 15:51:59 -0400

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+GitHub Repo for Django back end / REST API : https://github.com/johnpwilkinson/shoe_store_back_end
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shoes: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8000/api/shoe/")
+      .then((response) => response.json())
+      .then((data) => this.setState({ shoes: data }));
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Shoe Store Demo</h1>
+        <h2>Inventory System</h2>
+        {this.state.shoes.map((shoe) => (
+          <div
+            key={shoe.id}
+            style={{border: "black 2px solid", marginBottom: 10}}
+          >
+            <h3>Inventory number : {shoe.id}</h3>
+            <h4>Model : {shoe.brand_name}</h4>
+            <h4>Made By : {shoe.manufacturer}</h4>
+            <h4>Color : {shoe.color}</h4>
+            <h4>Size : {shoe.size}</h4>
+            <h4>Material : {shoe.material}</h4>
+            <h4>Type : {shoe.shoe_type}</h4>
+            <h4>Fastener : {shoe.fasten_type}</h4>
+          </div>
+        ))}
+      </>
+    );
+  }
 }
 
 export default App;
